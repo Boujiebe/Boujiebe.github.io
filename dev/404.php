1d1d8241add3c2052240a926350f2870e9f563a0
Kerim Garbouj <kerimgb@gmail.com>
2016-08-22 21:55:20 +0200

    Install Boujie starter kit

---
+++ dev/404.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ */
+
+get_header(); ?>
+
+	//No page found message here
+
+<?php get_footer(); ?>
---
+++ dev/assets/js/main.js
@@ -0,0 +1,9 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+(function(){
+	'use strict';
+
+$( document ).ready(function() {
+    
+});
+
+})(); // END OF USE STRICT FUNCTION
