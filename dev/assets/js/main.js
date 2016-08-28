2bfc9a49c39697307c5cd74b851ffd23b6101ed7
Kerim Garbouj <kerimgb@gmail.com>
2016-08-28 21:28:44 +0200

    desktop styling + finetuning

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -4,6 +4,10 @@
 
 $( document ).ready(function() {
 
+  $("#mobile-nav .s-menu, #site-nav li a").click(function(){
+    $("#site-nav").toggleClass("active");
+  });
+
 
 	// Smooth scrolling
 	
@@ -14,12 +18,74 @@ $( document ).ready(function() {
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html, body').animate({
-              scrollTop: target.offset().top
+              scrollTop: target.offset().top  - 80
             }, 1000);
             return false;
           }
         }
       });
+    });
+
+    // Sticky header
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1){  
+        $('#site-header, #mobile-nav').addClass("sticky");
+      }
+      else{
+        $('#site-header, #mobile-nav').removeClass("sticky");
+      }
+    });
+
+    // Cache selectors
+    var topMenu = $("#site-nav"),
+        topMenuHeight = topMenu.outerHeight()+15,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+          var item = $($(this).attr("href"));
+          if (item.length) { return item; }
+        });
+
+    // Bind to scroll
+    $(window).scroll(function(){
+       // Get container scroll position
+       var fromTop = $(this).scrollTop()+topMenuHeight;
+
+       // Get id of current scroll item
+       var cur = scrollItems.map(function(){
+         if ($(this).offset().top < fromTop)
+           return this;
+       });
+       // Get the id of the current element
+       cur = cur[cur.length-1];
+       var id = cur && cur.length ? cur[0].id : "";
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+    });​
+
+    
+    // headunit popup
+
+    $(".pop-up, .pop-up-overlay").hide();
+
+    $(".view-more-source").click(function(){
+        $(".pop-up-source, .pop-up-overlay").show();
+    });
+
+    $(".view-more-audio").click(function(){
+        $(".pop-up-audio, .pop-up-overlay").show();
+    });
+
+    $(".view-more-illumination").click(function(){
+        $(".pop-up-illumination, .pop-up-overlay").show();
+    });
+
+    $(".pop-up-overlay, .close").click(function(){
+        $(".pop-up-audio, .pop-up-illumination, .pop-up-source, .pop-up-overlay").hide();
     });
 
 });
