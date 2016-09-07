cda0e8ff76b42a76098a692819e967e079491d0f
Kerim Garbouj <kerimgb@gmail.com>
2016-09-07 08:49:42 +0200

    Illumination control

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -88,6 +88,56 @@ $( document ).ready(function() {
         $(".bk2210-pop-up-audio, .bk2210-pop-up-illumination, .bk2210-pop-up-source, .bk2210-pop-up-overlay").hide();
     });
 
+    // ilumination control
+
+    $(".bk2210-connection img").after("<i class='s-logo'></i>");
+    $(".bk2210-connection img").remove();
+
+    $("#bk2210-illuminaion-control").append( "<div class='bull-top'><i class='bull-white'></i><i class='bull-red'></i><i class='bull-orange'></i><i class='bull-light-orange'></i><i class='bull-yellow'></i><i class='bull-light-green active-selection'></i><i class='bull-green'></i><i class='bull-turquoise'></i><i class='bull-light-blue'></i><i class='bull-blue'></i><i class='bull-purple'></i><i class='bull-pink'></i></div>" );
+    $("#bk2210-illuminaion-control").append( "<div class='bull-bottom'><i class='bull-white'></i><i class='bull-red'></i><i class='bull-orange'></i><i class='bull-light-orange'></i><i class='bull-yellow'></i><i class='bull-light-green'></i><i class='bull-green'></i><i class='bull-turquoise'></i><i class='bull-light-blue'></i><i class='bull-blue'></i><i class='bull-purple'></i><i class='bull-pink'></i></div>" );
+    
+
+    $("i[class^='bull-']").click(function(){
+        var colorClass = $(this).attr("class");
+        $("#bk2210-custom-head-unit h2").attr("class", "").addClass("bk2210-title");
+        $(".bk2210-connection").attr("class", "").addClass("bk2210-connection");
+        $(".bk2210-circle-badge").attr("class", "").addClass("bk2210-circle-badge");
+
+        $("#bk2210-custom-head-unit h2, .bk2210-connection, .bk2210-circle-badge").addClass("color"+colorClass);
+
+        $(".bk2210-connection").next().attr("src", "dist/img/screens/pioneer-headunit-illumination-color"+colorClass+".png");
+
+        // $(".bk2210-connection").next().fadeOut("fast", function() {
+        //   $(this).attr("src", "dist/img/screens/pioneer-headunit-illumination-color"+colorClass+".png").fadeIn();
+        // });
+
+        $("i[class^='bull-']").removeClass("active-selection");
+        $(this).toggleClass("active-selection");
+    });
+
+    var windowWidth = $(window).width();
+    $("#bk2210-illuminaion-control #bk2210-ill-phone").attr("src", "dist/img/devices/arc-device-illumination-control-2.png");
+
+    if (windowWidth >= 600) {
+      $("#bk2210-illuminaion-control #bk2210-ill-phone").attr("src", "dist/img/devices/arc-device-illumination-control-2.png");
+    } else if (windowWidth < 600) {
+      $("#bk2210-illuminaion-control #bk2210-ill-phone").attr("src", "dist/img/devices/arc-device-illumination-control.png");
+    }
+
+    $( window ).resize(function() {
+      var windowWidth = $(window).width();
+
+      if (windowWidth >= 600) {
+        $("#bk2210-illuminaion-control #bk2210-ill-phone").attr("src", "dist/img/devices/arc-device-illumination-control-2.png");
+      } else if (windowWidth < 600) {
+        $("#bk2210-illuminaion-control #bk2210-ill-phone").attr("src", "dist/img/devices/arc-device-illumination-control.png");
+      }
+    });
+
+
+    $(".bk2210-bk2210-large-3").removeClass("bk2210-bk2210-large-3").addClass("bk2210-large-3");
+
+    
 });
 
 })(); // END OF USE STRICT FUNCTION
